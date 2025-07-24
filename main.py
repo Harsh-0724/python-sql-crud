e284d68039d970d5f6d1fa5961f571d88b59b55b
Harsh <141826318+Harsh-0724@users.noreply.github.com>
2025-07-24 17:40:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ try:
     conn = mysql.connector.connect(
         host = "127.0.0.1",
         user = "root",
-        password = os,
+        password = os.getenv("DB_PASSWORD"),
         database = "python_demo"
     )
 
